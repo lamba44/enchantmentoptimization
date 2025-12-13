d60b62685ba39989ea681ad78064107cfa73cf49
Saksham Lamba <sakshamlambamonu@gmail.com>
2025-12-13 18:32:59 +0530

    update: Vercel Analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./Routes";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <AppRoutes />
+        <Analytics />
     </React.StrictMode>
 );
